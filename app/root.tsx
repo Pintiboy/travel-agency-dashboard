cc10155c45c08d9170a9205bc7cac24fdc2acad9
Pintiboy <wurschtchef@hotmail.com>
2025-04-26 22:39:18 +0200

    routing

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,8 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import pkg from "@syncfusion/ej2-base";
+const { registerLicense } = pkg;
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -22,6 +24,8 @@ export const links: Route.LinksFunction = () => [
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
 ];
+
+registerLicense(import.meta.env.VITE_EJ2_LICENSE_KEY);
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
